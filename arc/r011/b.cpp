d35e0e64e94c62e7b95012fc1e65be98dd1afe60
clarinet758 <clarinet758@gmail.com>
2015-07-30 00:50:33 +0900

    test

---
+++ arc/r011/b.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+
+int main(){
+    int a,b;
+    double t=3.305785; 
+    scanf("%d %d",&a,&b);
+    printf("%.10lf\n",a*b/t);
+    return 0;
+}
