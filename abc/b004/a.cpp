8bc4880842463d392fe450cb72b3c85e252e2765
clarinet758 <clarinet758@gmail.com>
2015-07-30 01:09:38 +0900

    WAP1

---
+++ abc/b004/a.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("%d\n",a*2);
+    return 0;
+}
