b63183858962799b9181754964892d602c79e506
nyon <clarinet758@gmail.com>
2016-01-02 18:10:48 +0900

    mv_test

---
+++ arc/r001_025/r014/a.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf(a%2?"Red\n":"Blue\n");
+    return 0;
+}
