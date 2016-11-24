e27afe1aab269c889402bbc60184125882da70b4
nyon <clarinet758@gmail.com>
2016-11-25 01:23:32 +0900

    Bは何かが違う気がする

--- abc/b026_050/b040/b1.py
+++ abc/b026_050/b040/b1.py
@@ -1,39 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-import collections
-import bisect
-#sys.stdin=file('input.txt')
-#sys.stdout=file('output.txt','w')
-#10**9+7
-mod=1000000007
-#mod=1777777777
-pi=3.141592653589
-IS=float('inf')
-xy=[(1,0),(-1,0),(0,1),(0,-1)]
-bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
-def gcd(a,b): return a if b==0 else gcd(b,a%b)
-def lcm(a,b): return a*b/gcd(a,b)
-def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
-def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
 
-ans=IS
 n=int(raw_input())
-if n==1:
-    print 0
-    exit()
-
-for i in range(int(n**0.5),0,-1):
-    tmp=n/i
-    ans=min(ans,(n-i*tmp)+abs(i-tmp))
+ans=10**9
+tmp=int(n**0.5)
+for i in range(1,tmp+20):
+    for j in range(tmp+20,0,-1):
+        if 0<i*j<=n:
+            ans=min(ans,n-i*j+abs(i-j))
 print ans
-exit()
-n,k=map(int,raw_input().split())
-l=map(int,raw_input().split())
-#end = time.clock()
-#print end - start
