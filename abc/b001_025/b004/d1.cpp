703df92527468d7c1549c3846d095dd75f0e46a8
clarinet758 <clarinet758@gmail.com>
2016-12-17 19:02:45 +0900

    40点解法

--- abc/b001_025/b004/d1.cpp
+++ abc/b001_025/b004/d1.cpp
@@ -15,14 +15,23 @@ using namespace std;
  * sort(ar.begin(),ar.end())
  * int sum=accumulate(ar.begin(),ar.end(),0);
  **/
+int sol(int x) {
+    x-=1;
+    if (x<3) {
+        return max(0,x);
+    } else if (x%2==1){
+        return (1+x/2)*x/2+(x/4+1);
+    } else {
+        return (1+x/2)*x/2;
+    }
+}
 
 int main(){
-    map<int, int> mp;
-    mp={{1,0},{2,1},{3,2},{4,4},{5,6}};
     int mod=1000000007;
     int r,g,b;
     scanf("%d %d %d",&r,&g,&b);
-    printf("%d\n",mp[r]+mp[g]+mp[b]);
+
+    printf("%d\n",sol(r)+sol(g)+sol(b));
 
     return 0;
 }
