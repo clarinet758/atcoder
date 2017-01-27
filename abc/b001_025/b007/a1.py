831a2924cd77f88470902da1654dbd7932a62868
nyon <clarinet758@gmail.com>
2017-01-28 03:43:35 +0900

    Cは#で囲われてるので甘い

--- abc/b001_025/b007/a1.py
+++ abc/b001_025/b007/a1.py
@@ -1,14 +1,3 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-#start = time.clock()
-
-print int(raw_input())-1
-ans=chk=0
-#end = time.clock()
-#print end - start
+print input()-1
--- abc/b001_025/b007/b1.py
+++ abc/b001_025/b007/b1.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-#start = time.clock()
-ans=chk=0
+
 print -1 if raw_input()=='a' else 'a'
-#end = time.clock()
-#print end - start
