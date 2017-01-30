92e38b2db857bd3f8cb8bc904c5db11b49e07633
nyon <clarinet758@gmail.com>
2017-01-31 01:51:30 +0900

    Cは部分点

--- abc/b001_025/b008/a1.py
+++ abc/b001_025/b008/a1.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-#start = time.clock()
 s,t=map(int,raw_input().split())
 print t-s+1
-
-#end = time.clock()
-#print end - start
--- abc/b001_025/b008/b1.py
+++ abc/b001_025/b008/b1.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-#start = time.clock()
-n=int(raw_input())
-#n,k=map(int,raw_input().split())
-#l=[int(x) for x in raw_input().split()]
-ans=['',0]
+
+n=input()
+ans,cnt='',0
 d={}
 for i in range(n):
     s=raw_input()
@@ -18,9 +10,7 @@ for i in range(n):
         d[s]+=1
     else:
         d[s]=1
-    if d[s]>ans[1]:
-        ans=[s,d[s]]
-print ans[0]
-
-#end = time.clock()
-#print end - start
+    if  d[s]>cnt:
+        cnt=d[s]
+        ans=s
+print ans
