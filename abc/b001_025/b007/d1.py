062dfafb2832dba12bc5c8cd6832a00968188a27
nyon <clarinet758@gmail.com>
2017-01-30 23:21:58 +0900

    掃除

--- abc/b001_025/b007/d1.py
+++ abc/b001_025/b007/d1.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-#start = time.clock()
-#n=int(raw_input())
 a,b=map(int,raw_input().split())
 ans=0
-for i in xrange(a,b+1):
-    chk=str(i)
-    if '4' in chk or '9' in chk:
+if b>10000:
+    print 0
+    exit()
+for i in range(a,b+1):
+    i=str(i)
+    if '4' in i or '9' in i:
         ans+=1
 print ans
-#end = time.clock()
-#print end - start
