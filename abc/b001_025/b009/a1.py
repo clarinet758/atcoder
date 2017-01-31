d4e618117c1554f4e214c5bb6fdb0ff05a4fbccf
nyon <clarinet758@gmail.com>
2017-01-31 23:12:23 +0900

    あかんですわ

--- abc/b001_025/b009/a1.py
+++ abc/b001_025/b009/a1.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-#10**9+7
-mod=1000000007
-print(input()+1)/2
+
+print (input()+1)/2
--- abc/b001_025/b009/b1.py
+++ abc/b001_025/b009/b1.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-#10**9+7
-mod=1000000007
-#start = time.clock()
-n=int(raw_input())
-a=set([])
-for i in range(n):
-    a.add(int(raw_input()))
-a=list(a)
-a.sort()
-print a[-2]
 
-#end = time.clock()
-#print end - start
+n=input()
+w=set()
+for i in range(n):
+    w.add(input())
+w.remove(max(w))
+print max(w)
