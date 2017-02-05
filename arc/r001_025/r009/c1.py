9e27b8e9902febe76bdd072cb597c998688d93c2
nyon <clarinet758@gmail.com>
2017-02-06 01:53:13 +0900

    再帰わからん

--- arc/r001_025/r009/c1.py
+++ arc/r001_025/r009/c1.py
@@ -1,45 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-import collections
-#sys.stdin=file('input.txt')
-#sys.stdout=file('output.txt','w')
-#10**9+7
-#mod=1000000007
-mod=1777777777
-pi=3.141592653589
-xy=[(1,0),(-1,0),(0,1),(0,-1)]
-bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
-def gcd(a,b): return a if b==0 else gcd(b,a%b)
-def lcm(a,b): return a*b/gcd(a,b)
-def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
-def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
 
-#n=int(raw_input())
-N,K=map(int,raw_input().split())
-used=[0]*N
-def dfs(pos,used,nokori):
-    if pos==N:
-        return 1 if nokori==0 else 0
+mod=1777777777
+def sol(pos, use, nokori, n):
+    #残った人数の確認
+    if pos==n:
+        if nokori==0: return 1
+        else: return 0
+
     ret=0
-    for i in range(N):
-        if used[i]:
-            continue
-        used[i]=1
-        nextnokori=nokori
+    #pos番目の人が誰の手紙を受け取ったか調べる
+    for i in range(n):
+        if use[i]: continue
+        #手紙の使用フラグ
+        use[i]=1
+
+        #残りの間違える個数を更新
+        tmp=nokori
         if i!=pos:
-            nextnokori-=1
-        ret+=dfs(pos+1,used,nextnokori)
-        used[i]=0
+            tmp-=1
+        ret+=sol(pos+1, use, tmp, n)
+        use[i]=0
     return ret
-print dfs(0,used,K) if N<9 else 0
-exit()
-l=map(int,raw_input().split())
-ans=chk=0
-#end = time.clock()
-#print end - start
+
+
+
+n,k=map(int,raw_input().split())
+use=[0]*n
+
+if n>100: print 0
+else: print sol(0, use, k, n)
