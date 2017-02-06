a600160f7ac15641a747790cf6c50701f4be25f0
nyon <clarinet758@gmail.com>
2017-02-07 01:12:18 +0900

    整理

--- arc/r001_025/r005/c1.py
+++ arc/r001_025/r005/c1.py
@@ -5,59 +5,30 @@ xy=[(1,0),(-1,0),(0,1),(0,-1)]
 
 h,w=map(int,raw_input().split())
 c=[]
-s0=set()
-s1=set()
-s2=set()
+s=[set() for i in range(4)]
 
-def yes():
-    print 'YES'
-    exit()
 
 for i in range(h):
     tmp=list(raw_input())
     if 's' in tmp:
-        s0.add((i,tmp.index('s')))
-        tmp[tmp.index('s')]=0
+        s[0].add((i,tmp.index('s')))
     c.append(tmp)
 
-while len(s0):
-    t=s0.pop()
-    for y,x in xy:
-        ty=t[0]+y
-        tx=t[1]+x
-        if 0<=ty<h and 0<=tx<w:
-            if c[ty][tx]=='.':
-                c[ty][tx]=0
-                s0.add((ty,tx))
-            elif c[ty][tx]=='#':
-                c[ty][tx]=1
-                s1.add((ty,tx))
-            elif c[ty][tx]=='g':
-                yes()
+def sol(v):
+    while len(s[v]):
+        t=s[v].pop()
+        for y,x in xy:
+            ty,tx=t[0]+y,t[1]+x
+            if 0<=ty<h and 0<=tx<w:
+                if c[ty][tx]=='.' or c[ty][tx]=='#':
+                    s[v+[0,1][c[ty][tx]=='#']].add((ty,tx))
+                    c[ty][tx]=v+[0,1][c[ty][tx]=='#']
+                elif c[ty][tx]=='g':
+                    print 'YES'
+                    exit()
 
-while len(s1): 
-    t=s1.pop()
-    for y,x in xy:
-        ty=t[0]+y
-        tx=t[1]+x
-        if 0<=ty<h and 0<=tx<w:
-            if c[ty][tx]=='.':
-                c[ty][tx]=1
-                s1.add((ty,tx))
-            elif c[ty][tx]=='#':
-                c[ty][tx]=2
-                s2.add((ty,tx))
-            elif c[ty][tx]=='g':
-                yes()
-while len(s2):
-    t=s2.pop()
-    for y,x in xy:
-        ty=t[0]+y
-        tx=t[1]+x
-        if 0<=ty<h and 0<=tx<w:
-            if c[ty][tx]=='.':
-                c[ty][tx]=2
-                s2.add((ty,tx))
-            elif c[ty][tx]=='g':
-                yes()
+for i in range(3):
+    sol(i)
+
 print 'NO'
+
