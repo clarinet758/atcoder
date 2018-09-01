9fbe75ee5bf3dfd3deae93239f5441218228a41b
clarinet758 <clarinet758@gmail.com>
2018-09-01 22:41:42 +0900

    oo--

---
+++ abc/b101_125/b108/a1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+k=int(input())
+print(k//2*(k//2+k%2))
---
+++ abc/b101_125/b108/b1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+x1,y1,x2,y2=map(int,input().split())
+print(x2+(y1-y2),y2-(x1-x2),x1+(y1-y2),y1-(x1-x2))
+#print(x2+(y1-y2),y2-(x1-x2))
