14e38dd2ba2b4690414f9ad521ca9bf2c6468a94
clarinet758 <clarinet758@gmail.com>
2018-10-12 01:26:24 +0900

    kuyac

--- abc/b076_100/b097/c1.py
+++ abc/b076_100/b097/c1.py
@@ -6,9 +6,9 @@ k=int(input())
 n=len(s)
 d=set()
 #d={s[i:j+1] for j in range(i,n) for i in range(n)}
-d={s[i:j+1] for i in range(n) for j in range(i,n)}
-#for i in range(n):
-#    for j in range(i,n):
-#        d.add(s[i:j+1])
+#d={s[i:j+1] for i in range(n) for j in range(i,n)}
+for i in range(n):
+    for j in range(i,max(n,i+5)):
+        d.add(s[i:j+1])
 ans=sorted(list(d))
 print(ans[k-1])
