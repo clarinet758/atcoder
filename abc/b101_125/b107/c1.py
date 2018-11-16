efbe0040283798c57b6aaae433efecc839175c44
clarinet758 <clarinet758@gmail.com>
2018-11-17 02:53:24 +0900

    dame

--- abc/b101_125/b107/c1.py
+++ abc/b101_125/b107/c1.py
@@ -1,37 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+#WAA TLEEEEEEEEEE
 n,k=map(int,input().split())
 x=[int(i) for i in input().split()]
 l,r=[],[]
-for i in x:
-    if i<0: l.append(-i)
-    if i>=0: r.append(i)
-l.sort()
-r.sort()
-l=l[:n]
-r=r[:n]
+for i in range(n):
+    if x[i]<0 and i+1<n: l.append(abs(x[i]-min(x[i+1],0)))
+    elif x[i]<0: l.append(-x[i])
+    if x[i]>=0 and i>0: r.append(x[i]-max(0,x[i-1]))
+    elif x[i]>=0: r.append(x[i])
+
+l=l[::-1]
+l=l[:k]
+r=r[:k]
 ll=len(l)
 rr=len(r)
-lx,rx=[],[]
-for i in range(ll):
-    if i==0: lx.append(l[i])
-    else: lx.append(l[i]-l[i-1])
-for i in range(rr):
-    if i==0: rx.append(r[i])
-    else: rx.append(r[i]-r[i-1])
 
-ans=sum(lx)*2+sum(rx)*2
-for i in range(ll):
-    if n-1==i: ans=min(ans,sum(lx))
-    elif i+rr+1>=n: ans=min(ans,sum(lx[:i+1])*2+sum(rx[:n-i]))
-    print("#",lx[:i+1],rx[:n-i])
+ans=sum(l)*2+sum(r)*2
+if ll==k: ans=min(ans,sum(l))
+if rr==k: ans=min(ans,sum(r))
+for i in range(1,k):
+    if rr>=k-(ll-i): ans=min(ans,(sum(l[:-i])*2)+sum(r[:k-(ll-i)]))
+    if ll>=k-(rr-i): ans=min(ans,(sum(r[:-i])*2)+sum(l[:k-(rr-i)]))
 
 print(ans)
-for i in range(rr):
-    if n-1==i: ans=min(ans,sum(r))
-    elif i+ll+1>=n: ans=min(ans,sum(rx[:i+1])*2+sum(lx[:n-i]))
-    print("#",rx[:i+1],lx[:n-i])
 
+"""
+    print(rr,i,"#",len(l[:-i]),i,k)
+    if k-1==i: ans=min(ans,sum(l))
+    elif i+rr+1>=k: ans=min(ans,sum(l[:i+1])*2+sum(r[:k-i-1]))
+    if k-1==i: ans=min(ans,sum(r))
+    elif i+ll+1>=k: ans=min(ans,sum(r[:i+1])*2+sum(l[:k-i-1]))
+
+exit()
+for i in range(ll):
+    if k-1==i: ans=min(ans,sum(l))
+    elif i+rr+1>=k: ans=min(ans,sum(l[:i+1])*2+sum(r[:k-i-1]))
+
+for i in range(rr):
+    if k-1==i: ans=min(ans,sum(r))
+    elif i+ll+1>=k: ans=min(ans,sum(r[:i+1])*2+sum(l[:k-i-1]))
 print(ans)
     #if i+rr+1>=n and i+1<n:
+   # print(l[:i+1],r[:k-i-1])
+    #print(r[:i+1],l[:k-i-1])
+"""
