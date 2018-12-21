de6e2917d80819529704ffc5109fcb77b5648fb3
clarinet758 <clarinet758@gmail.com>
2018-12-22 02:49:03 +0900

    2to3

--- arc/r001_025/r001/c1.py
+++ arc/r001_025/r001/c1.py
@@ -1,76 +1,38 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-import collections
-#sys.stdin=file('input.txt')
-#sys.stdout=file('output.txt','w')
-#10**9+7
-mod=1000000007
-#mod=1777777777
-pi=3.141592653589
-xy=[(1,0),(-1,0),(0,1),(0,-1)]
-bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
-def gcd(a,b): return a if b==0 else gcd(b,a%b)
-def lcm(a,b): return a*b/gcd(a,b)
-def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
-def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
 
-l=[]
-q=0
-for i in range(8):
-    tmp=list(raw_input())
-    q+=tmp.count('Q')
-    l.append(tmp)
-
-def chk(y,x):
-    for i in range(8):
-        if i!=x and l[y][i]=='Q':
-            return 0
-        if i!=y and l[i][x]=='Q':
-            return 0
+def chk(h,w):
     for i in range(1,8):
-        xm,xp,ym,yp=x-i,x+i,y-i,y+i
-        if ym>=0 and xm>=0 and l[ym][xm]=='Q':
-            return 0
-        if ym>=0 and xp<8 and l[ym][xp]=='Q':
-            return 0
-        if yp<8 and xm>=0 and l[yp][xm]=='Q':
-            return 0
-        if yp<8 and xp<8 and l[yp][xp]=='Q':
-            return 0
+        if 0<=1*i+h<8 and 0<=0*i+w<8 and b[1*i+h][0*i+w]=="Q": return 0
+        if 0<=-1*i+h<8 and 0<=0*i+w<8 and b[-1*i+h][0*i+w]=="Q": return 0
+        if 0<=0*i+h<8 and 0<=-1*i+w<8 and b[0*i+h][-1*i+w]=="Q": return 0
+        if 0<=0*i+h<8 and 0<=1*i+w<8 and b[0*i+h][1*i+w]=="Q": return 0
+        if 0<=-1*i+h<8 and 0<=-1*i+w<8 and b[-1*i+h][-1*i+w]=="Q": return 0
+        if 0<=1*i+h<8 and 0<=-1*i+w<8 and b[1*i+h][-1*i+w]=="Q": return 0
+        if 0<=-1*i+h<8 and 0<=1*i+w<8 and b[-1*i+h][1*i+w]=="Q": return 0
+        if 0<=1*i+h<8 and 0<=1*i+w<8 and b[1*i+h][1*i+w]=="Q": return 0
     return 1
 
-def dfs(nokori,cy):
-    if nokori==0:
-        for i in l:
-            print ''.join(i)
+def dfs(c,g):
+    if c==0:
+        for i in b: print(*i, sep="")
         exit()
-
-    for y in range(cy,8):
+    for y in range(g,8):
         for x in range(8):
-            if l[y][x]=='Q' or chk(y,x)==0:
-                continue
-            l[y][x]='Q'
-            dfs(nokori-1,y+1)
-            l[y][x]='.'
-
+            if b[y][x]=="Q" or chk(y,x)==0: continue
+            b[y][x]="Q"
+            dfs(c-1,g+1)
+            b[y][x]="."
+q=0
+b=[]
+for i in range(8):
+    tmp=list(input())
+    q+=tmp.count("Q")
+    b.append(tmp)
 for i in range(64):
-    if l[i/8][i%8]=='Q' and chk(i/8,i%8)==0:
-        print 'No Answer'
+    if b[i//8][i%8]=="Q" and chk(i//8,i%8)==0:
+        print("No Answer")
         exit()
 q=8-q
 dfs(q,0)
-print 'No Answer'
-
-exit()
-n=int(raw_input())
-n,k=map(int,raw_input().split())
-#l=map(int,raw_input().split())
-ans=chk=0
-#end = time.clock()
-#print end - start
+print("No Answer")
