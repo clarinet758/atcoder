77dbb7b92b110ce085e0b0ad16e0823b28789bcb
clarinet758 <clarinet758@gmail.com>
2018-12-22 02:47:35 +0900

    2AC

--- arc/r001_025/r001/c1.py
+++ arc/r001_025/r001/c1.py
@@ -1,74 +1,76 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-#8個くらいWA出る
+import time
+import sys
+import io
+import re
+import math
+import itertools
+import collections
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def gcd(a,b): return a if b==0 else gcd(b,a%b)
+def lcm(a,b): return a*b/gcd(a,b)
+def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
 
-def chk(b,y,x):
+l=[]
+q=0
+for i in range(8):
+    tmp=list(raw_input())
+    q+=tmp.count('Q')
+    l.append(tmp)
+
+def chk(y,x):
+    for i in range(8):
+        if i!=x and l[y][i]=='Q':
+            return 0
+        if i!=y and l[i][x]=='Q':
+            return 0
     for i in range(1,8):
-        #縦チェック
-        if b[(y+i)%8][x]=='Q':
+        xm,xp,ym,yp=x-i,x+i,y-i,y+i
+        if ym>=0 and xm>=0 and l[ym][xm]=='Q':
             return 0
-        #横チェック
-        if b[y][(x+i)%8]=='Q':
+        if ym>=0 and xp<8 and l[ym][xp]=='Q':
             return 0
-        #右下チェック
-        if 0<=y+i<8 and 0<=x+i<8 and b[y+i][x+i]=='Q':
+        if yp<8 and xm>=0 and l[yp][xm]=='Q':
             return 0
-        #左上チェック
-        if 0<=y-i<8 and 0<=x-i<8 and b[y-i][x-i]=='Q':
-            return 0
-        #右上チェック
-        if 0<=y-i<8 and 0<=x+i<8 and b[y-i][x+i]=='Q':
-            return 0
-        #左下チェック
-        if 0<=y+i<8 and 0<=x-i<8 and b[y+i][x-i]=='Q':
+        if yp<8 and xp<8 and l[yp][xp]=='Q':
             return 0
     return 1
 
-def sol(b,f,cnt):
-    if cnt==8:
-        return 1
-
-#f行目に既にQがあったら他のQと干渉がないかをチェック
-#干渉があればfalse扱いで終了、干渉がなければf+1で次の行へ
-    if 'Q' in b[f]:
-        if chk(b,f,b[f].index('Q'))==0:
-            return 0
-        else:
-            sol(b,f+1,cnt)
-
-#f行目にQがなければ0番目からQを仮に置いて干渉がなければ次の行へ
-#干渉があれば仮置きのQを.に戻して同じf行目の右のマスでチェックする
-    else:
-        for p in range(8):
-            b[f][p]='Q'
-            cnt+=1
-            if chk(b,f,p):
-                sol(b,f+1,cnt)
-            else:
-                b[f][p]='.'
-                cnt-=1
-
-
-b=[]
-cnt=0
-for a in range(8):
-    tmp=raw_input()
-    cnt+=tmp.count('Q')
-    if tmp.count('Q')>1:
-        print 'No Answer'
-        exit()
-    b.append(list(tmp))
-
-#初期配置で干渉があったらNoで終了
-for a,i in enumerate(b):
-    if 'Q' in i and chk(b,y,i.index('Q'))==0:
-        print 'No Answer'
+def dfs(nokori,cy):
+    if nokori==0:
+        for i in l:
+            print ''.join(i)
         exit()
 
-sol(b,0,cnt)
-for a,i in enumerate(b):
-    if 'Q' not in i or chk(b,a,i.index('Q'))==0:
+    for y in range(cy,8):
+        for x in range(8):
+            if l[y][x]=='Q' or chk(y,x)==0:
+                continue
+            l[y][x]='Q'
+            dfs(nokori-1,y+1)
+            l[y][x]='.'
+
+for i in range(64):
+    if l[i/8][i%8]=='Q' and chk(i/8,i%8)==0:
         print 'No Answer'
         exit()
-for i in b:
-    print ''.join(i)
+q=8-q
+dfs(q,0)
+print 'No Answer'
+
+exit()
+n=int(raw_input())
+n,k=map(int,raw_input().split())
+#l=map(int,raw_input().split())
+ans=chk=0
+#end = time.clock()
+#print end - start
