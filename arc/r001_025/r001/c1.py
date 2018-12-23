da4c41da60f1e81c50f32d9f44c8fddbb5a4c17a
clarinet758 <clarinet758@gmail.com>
2018-12-24 01:14:32 +0900

    hoo

--- arc/r001_025/r001/c1.py
+++ arc/r001_025/r001/c1.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-def chk(h,w):
+def chk(y,x):
     for i in range(1,8):
-        if 0<=1*i+h<8 and 0<=0*i+w<8 and b[1*i+h][0*i+w]=="Q": return 0
-        if 0<=-1*i+h<8 and 0<=0*i+w<8 and b[-1*i+h][0*i+w]=="Q": return 0
-        if 0<=0*i+h<8 and 0<=-1*i+w<8 and b[0*i+h][-1*i+w]=="Q": return 0
-        if 0<=0*i+h<8 and 0<=1*i+w<8 and b[0*i+h][1*i+w]=="Q": return 0
-        if 0<=-1*i+h<8 and 0<=-1*i+w<8 and b[-1*i+h][-1*i+w]=="Q": return 0
-        if 0<=1*i+h<8 and 0<=-1*i+w<8 and b[1*i+h][-1*i+w]=="Q": return 0
-        if 0<=-1*i+h<8 and 0<=1*i+w<8 and b[-1*i+h][1*i+w]=="Q": return 0
-        if 0<=1*i+h<8 and 0<=1*i+w<8 and b[1*i+h][1*i+w]=="Q": return 0
+        if 0<=1*i+y<8 and c[1*i+y][x]=="Q": return 0
+        if 0<=1*i+x<8 and c[y][1*i+x]=="Q": return 0
+        if 0<=-1*i+y<8 and c[-1*i+y][x]=="Q": return 0
+        if 0<=-1*i+x<8 and c[y][-1*i+x]=="Q": return 0
+        if 0<=1*i+y<8 and 0<=-1*i+x<8 and c[1*i+y][-1*i+x]=="Q": return 0
+        if 0<=-1*i+y<8 and 0<=-1*i+x<8 and c[-1*i+y][-1*i+x]=="Q": return 0
+        if 0<=-1*i+y<8 and 0<=1*i+x<8 and c[-1*i+y][1*i+x]=="Q": return 0
+        if 0<=1*i+y<8 and 0<=1*i+x<8 and c[1*i+y][1*i+x]=="Q": return 0
     return 1
 
-def dfs(c,g):
-    if c==0:
-        for i in b: print(*i, sep="")
+def test():
+    for i in range(64):
+        if c[i//8][i%8]=="Q" and chk(i//8,i%8)==0:
+            print("No Answer")
+            exit()
+    return 1
+
+def sol(q):
+    if q==0 and test():
+        for i in c:
+            print(*i, sep="")
         exit()
-    for y in range(g,8):
-        for x in range(8):
-            if b[y][x]=="Q" or chk(y,x)==0: continue
-            b[y][x]="Q"
-            dfs(c-1,g+1)
-            b[y][x]="."
-q=0
-b=[]
+    for i in range(64):
+        if c[i//8][i%8]=="Q" or chk(i//8,i%8)==0: continue
+        c[i//8][i%8]="Q"
+        sol(q-1)
+        c[i//8][i%8]="."
+
+c=[]
+q=8
 for i in range(8):
-    tmp=list(input())
-    q+=tmp.count("Q")
-    b.append(tmp)
-for i in range(64):
-    if b[i//8][i%8]=="Q" and chk(i//8,i%8)==0:
+    t=input()
+    if t.count("Q")>1:
         print("No Answer")
         exit()
-q=8-q
-dfs(q,0)
-print("No Answer")
+    if "Q" in t: q-=1
+    c.append(list(t))
+sol(q)
