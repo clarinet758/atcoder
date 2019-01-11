34033014d5b72654f443df278b9b72f6ed94e9be
clarinet758 <clarinet758@gmail.com>
2019-01-12 02:42:04 +0900

    hoaa

--- etc/edpc/e1.cpp
+++ etc/edpc/e1.cpp
@@ -2,20 +2,27 @@
 using namespace std;
 
 int main(){
-    int n,w;
-    scanf("%d %d",&n,&w);
-    long long dp[w+1]={0};
-    long long ans=0;
-    for (int  i=0;i<n;i++) {
-        int a;
-        long long b;
-        scanf("%d %lld",&a,&b);
-        for (long long j=w-a;j>0;j--) {
-            if (dp[j]!=0) dp[j+a]=max(dp[j+a],dp[j]+b);
+    int mod=1000000007;
+    long long n,w,ans=0;
+    scanf("%lld %lld",&n,&w);
+    long long dp[n+1][1000*n+1];
+    for (int i=0;i<=n;i++) for (int j=0;j<=1000*n;j++) dp[i][j]=mod;
+    for (int i=1;i<=n;i++) {
+        long long a,b;
+        scanf("%lld %lld",&a,&b);
+        dp[i][b]=min(dp[i][b],a);
+        for (int j=1;j<=1000*n-b;j++) {
+            if (dp[i-1][j]<w) {
+                dp[i][j]=min(dp[i][j],dp[i-1][j]);
+                dp[i][j+b]=min(dp[i][j+b], dp[i-1][j]+a);
+            }
         }
-        dp[a]=max(dp[a],b);
     }
-    for (int i=0;i<w+1;i++) ans=max(ans,dp[i]);
+    for (int i=0;i<=n;i++) for (long long j=0;j<=1000*n;j++) {
+        if (dp[i][j]<=w) {
+            ans=max(ans,j);
+        }
+    }
     printf("%lld\n",ans);
     return 0;
 }
