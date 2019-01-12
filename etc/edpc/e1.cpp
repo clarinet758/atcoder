7dc564dd4dbb80d87cd15a5416fbbca92cc10c2c
clarinet758 <clarinet758@gmail.com>
2019-01-12 18:35:56 +0900

    simple

--- etc/edpc/e1.cpp
+++ etc/edpc/e1.cpp
@@ -3,26 +3,21 @@ using namespace std;
 
 int main(){
     int mod=1000000007;
-    long long n,w,ans=0;
-    scanf("%lld %lld",&n,&w);
-    long long dp[n+1][1000*n+1];
-    for (int i=0;i<=n;i++) for (int j=0;j<=1000*n;j++) dp[i][j]=mod;
-    for (int i=1;i<=n;i++) {
-        long long a,b;
-        scanf("%lld %lld",&a,&b);
-        dp[i][b]=min(dp[i][b],a);
-        for (int j=1;j<=1000*n-b;j++) {
-            if (dp[i-1][j]<w) {
-                dp[i][j]=min(dp[i][j],dp[i-1][j]);
-                dp[i][j+b]=min(dp[i][j+b], dp[i-1][j]+a);
-            }
-        }
+    int n,w,ans=0;
+    scanf("%d %d",&n,&w);
+    int x=n*1000+1;
+    int dp[x];
+    for (int i=1;i<x;i++) dp[i]=mod;
+    for (int i=0;i<n;i++) {
+        int a,b;
+        scanf("%d %d",&a,&b);
+        for (int j=x-1-b;j>0;j--) if (dp[j]<mod) dp[j+b]=min(dp[j+b],dp[j]+a);
+        dp[b]=min(dp[b],a);
     }
-    for (int i=0;i<=n;i++) for (long long j=0;j<=1000*n;j++) {
-        if (dp[i][j]<=w) {
-            ans=max(ans,j);
-        }
+    for (int i=x-1;i>0;i--) if (dp[i]<=w) {
+        ans=i;
+        break;
     }
-    printf("%lld\n",ans);
+    printf("%d\n",ans);
     return 0;
 }
