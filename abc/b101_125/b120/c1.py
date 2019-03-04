2d63862b7284da58a32b9dcec1e0795ed3591be6
clarinet758 <clarinet758@gmail.com>
2019-03-05 01:16:21 +0900

    01どちらかしか残らないのでループ必要なし

--- abc/b101_125/b120/c1.py
+++ abc/b101_125/b120/c1.py
@@ -2,13 +2,4 @@
 # -*- coding: UTF-8 -*-
 
 n=input()
-ans=0
-chk=""
-for i in n:
-    if chk=="" or chk[-1]==i:
-        chk+=i
-    else:
-        chk=chk[:-1]
-        ans+=2
-    
-print(ans)
+print(len(n)-abs(n.count("0")-n.count("1")))
