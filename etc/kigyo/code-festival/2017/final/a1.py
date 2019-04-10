df970861c9ccf82844055e6edead0a72e059e8f8
clarinet758 <clarinet758@gmail.com>
2019-04-11 02:19:04 +0900

    q

---
+++ etc/kigyo/code-festival/2017/final/a1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+w=[ "AKIHABARA","KIHABARA","AKIHBARA","AKIHABRA","AKIHABAR","KIHBARA","KIHABRA","KIHABAR","AKIHBRA","AKIHBAR","AKIHABR","KIHBRA","KIHBAR","KIHABR","AKIHBR","KIHBR"]
+print(["NO","YES"][input() in w])
