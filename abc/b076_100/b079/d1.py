b0fcc5395b83eecd9b97110dc50da140f329dcd8
clarinet758 <clarinet758@gmail.com>
2019-05-25 16:06:47 +0900

    oops

--- abc/b076_100/b079/d1.py
+++ abc/b076_100/b079/d1.py
@@ -3,34 +3,17 @@
 
 def sol():
     h,w=map(int,input().split())
-    c=[]
-    for i in range(10):
-        l=[int(i) for i in input().split()]
-        c.append(l)
-    a=[]
-    for i in range(h):
-        l=[int(i) for i in input().split()]
-        a.append(l)
-    p=[0]*10
-    for i in range(10):
-        if i==1: continue
-        d=[1003]*10
-        d[i]=0
-        q=set([i])
-        while 1:
-            if len(q)==0: break
-            x=q.pop()
+    c=[list(map(int,input().split())) for i in range(10)]
+    a=[list(map(int,input().split())) for i in range(h)]
+    for k in range(10):
+        for i in range(10):
             for j in range(10):
-                if i==j: continue
-                if (d[j]>d[x]+c[x][j]):
-                    d[j]=d[x]+c[x][j]
-                    q.add(j)
-        p[i]=d[1] 
+                c[i][j]=min(c[i][j],c[i][k]+c[k][j])
     ans=0
     for i in range(h):
         for j in range(w):
             if a[i][j]==-1: pass
-            else: ans+=p[a[i][j]]
+            else: ans+=c[a[i][j]][1]
     print(ans)
 
 if __name__=="__main__":
