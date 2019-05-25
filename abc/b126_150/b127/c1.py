fef0134b28b70e6919cdfc5248b447fd4a41f74b
clarinet758 <clarinet758@gmail.com>
2019-05-26 01:52:06 +0900

    short

--- abc/b126_150/b127/c1.py
+++ abc/b126_150/b127/c1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-
 n,m=map(int,input().split())
 d=[1,n]
 for i in range(m):
@@ -9,14 +8,3 @@ for i in range(m):
     d[0]=max(d[0],l)
     d[1]=min(d[1],r)
 print(max(0,(d[1]-d[0]+1)))
-
-"""
-
-3-6
-
-4-5
-
-2-8
-5-9
-
-"""
