7d49ad825aa282c58f9d8082c96c125aa2f78427
clarinet758 <clarinet758@gmail.com>
2019-05-27 21:32:53 +0900

    short

--- abc/b126_150/b128/b1.py
+++ abc/b126_150/b128/b1.py
@@ -2,25 +2,9 @@
 # -*- coding: UTF-8 -*-
 
 n=int(input())
-c,m=[],[]
-d={}
-t={}
+ans=[]
 for i in range(n):
     s,p=map(str,input().split())
-    m.append((s,int(p)))
-    if s not in c:
-        c.append(s)
-        d[s]=[int(p)]
-    else:
-        d[s].append(int(p))
-    t[(s,int(p))]=i+1
-c.sort()
-ans={}
-cnt=1
-for i in c:
-    d[i].sort(reverse=True)
-    for j in d[i]:
-        ans[cnt]=(i,j)
-        cnt+=1
-for i in range(1,n+1):
-    print(t[ans[i]])
+    ans.append((s,int(p)*-1,i+1))
+ans.sort()
+for i in ans: print(i[2])
