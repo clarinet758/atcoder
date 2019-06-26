e28cc283f1af4a49bbedd20744cd2273a9d5d28c
clarinet758 <clarinet758@gmail.com>
2019-06-26 23:23:43 +0900

    hoe

--- etc/kigyo/colopl/2018/final/a1.py
+++ etc/kigyo/colopl/2018/final/a1.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-#WA 
-#100
-#A
-
-
 n=int(input())
 s=input()
-ans=chk=p=0
-x=len(s)
-s+=s
-for i in range(x*2):
-    if s[i]=="A": p+=1
-    else: p=0
-    print(p)
-    if i<x: ans+=p
-    else: chk+=p
-print(ans+(chk*(n-1)))
+d=[]
+ans=chk=t=0
+for i in s:
+    if i=="A":
+        chk+=1
+    elif i=="B" and chk>0:
+        d.append(chk)
+        d.append(0)
+        chk=0
+    elif i=="B" and len(d)==0:
+        d.append(0)
+if chk>0: d.append(chk)
+if d[0]==0 or d[-1]==0:
+    for j in d:
+        ans+=(j+1)*j//2
+    print(ans*n)
+    exit()
+tmp=[]
+if len(d)<6:
+    for i in range(n):
+        for j in d:
+            if j==0:
+                ans+=(t+1)*t//2
+                t=0
+            else:
+                t+=j
+    ans+=(t+1)*t//2
+else:
+    tmp.append(d[0])
+    tmp.append(0)
+    for i in d[2:-2]:
+        tmp[-1]+=(i+1)*i//2
+    tmp.append(d[-1])
+    for i in range(n):
+        for j in range(2):
+            if i==j==0:
+                ans+=(tmp[0]+1)*tmp[0]//2
+            elif j==0:
+                ans+=(tmp[0]+tmp[2]+1)*(tmp[0]+tmp[2])//2
+            elif j==1:
+                ans+=tmp[1]
+    ans+=(tmp[2]+1)*tmp[2]//2
+print(ans)
