b35defdc53c647bfd5e30fc9b3aafa79cfa9b5ca
clarinet758 <clarinet758@gmail.com>
2019-07-06 23:48:25 +0900

    gue

--- abc/b026_050/b045/c1.py
+++ abc/b026_050/b045/c1.py
@@ -1,48 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
-import math
-import itertools
-import collections
-import bisect
-#sys.stdin=file('input.txt')
-#sys.stdout=file('output.txt','w')
-#10**9+7
-mod=1000000007
-#mod=1777777777
-pi=3.141592653589
-IS=float('inf')
-xy=[(1,0),(-1,0),(0,1),(0,-1)]
-bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
-def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
-def gcd(a,b): return a if b==0 else gcd(b,a%b)
-def lcm(a,b): return a*b/gcd(a,b)
-def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
-def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
 
-s=list(raw_input())
-n=2**(len(s)-1)
+s=input()
 ans=chk=0
-for i in range(n):
-    tmp=bin(i)[2:]
-    tmp='0'*(n-len(tmp))+tmp
-    print tmp
-    t=''
-    for p,j in enumerate(s):
-        t+=j
-        if p!=n and tmp[p]=='1':
-            t+='+'
-    print t
-    ans+=eval(t)
-print ans
-exit()
+for i in range(2**(len(s)-1)):
+    d=bin(i)[2:]
+    d=("0"*(9-len(d))+d)[::-1]
+    t=int(s[0])
+    for i in range(len(s)-1):
+        if d[i]=="0":
+            t*=10
+            t+=int(s[i+1])
+        else:
+            ans+=t
+            t=int(s[i+1])
+    ans+=t
 
-
-
-n,k=map(int,raw_input().split())
-l=map(int,raw_input().split())
-#end = time.clock()
-#print end - start
+print(ans)
