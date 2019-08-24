b7741502230b9b187c576fde2d21c3fede95773a
clarinet758 <clarinet758@gmail.com>
2019-08-25 02:03:04 +0900

    goe

--- abc/b051_075/b062/c1.py
+++ abc/b051_075/b062/c1.py
@@ -3,13 +3,25 @@
 
 h,w=map(int,input().split())
 ans=h*w
-for x,y in ((h,w),(w,h)):
-    x1=x//3
-    x2=(x+2)//3
-    y2=(y+1)//2
-    ans=min(ans,abs(y*x2-y*x1))
-    t=(y*x1,y2*(x-x1),(y-y2)*(x-x1))
-    ans=min(ans,max(t)-min(t))
-    t=(y*x2,y2*(x-x2),(y-y2)*(x-x2))
-    ans=min(ans,max(t)-min(t))
+if h==w==2:
+    print(1)
+    exit()
+if h>2:
+    t=[h//3*w,(h-(h//3))//2*w,(h-(h//3)+1)//2*w]
+    t.sort()
+    ans=min(ans,t[2]-t[0])
+if w>2:
+    t=[w//3*h,(w-(w//3))//2*h,(w-(w//3)+1)//2*h]
+    t.sort()
+    ans=min(ans,t[2]-t[0])
+a,b=h,w
+for i in range(max(1,a//3-1),min(a//2+1,a-1)):
+    t=[i*b,(a-i)*(b//2),(a-i)*((b+1)//2)]
+    t.sort()
+    ans=min(ans,t[2]-t[0])
+a,b=w,h
+for i in range(max(1,a//3-1),min(a//2+1,a-1)):
+    t=[i*b,(a-i)*(b//2),(a-i)*((b+1)//2)]
+    t.sort()
+    ans=min(ans,t[2]-t[0])
 print(ans)
