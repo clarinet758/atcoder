d6f29a29fea629f990216cad2cef88d78ca31133
clarinet758 <clarinet758@gmail.com>
2019-08-27 23:32:12 +0900

    hoo

--- abc/b001_025/b006/d1.py
+++ abc/b001_025/b006/d1.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-import bisect
+from bisect import bisect_left
 
-def lis(c):
-    l=[]
-    l.append(c[0])
-    ans=1
-    for i in range(1,n):
-        if l[ans-1]<c[i]:
-            l.append(c[i])
-            ans+=1
-        else:
-            tmp=bisect.bisect_right(l,c[i])
-            l[tmp]=c[i]
-    return ans
-
-n=int(raw_input())
-N=range(1,n)
-c=[]
-for i in range(n):
-    c.append(int(raw_input()))
-print n-lis(c)
-
+n=int(input())
+c=[int(input()) for i in range(n)]
+w=[c[0]]
+for i in range(1,n):
+    if w[-1]<c[i]:
+        w.append(c[i])
+    else:
+        w[bisect_left(w,c[i])]=c[i]
+print(n-len(w))
