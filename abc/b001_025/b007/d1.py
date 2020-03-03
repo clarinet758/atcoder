903b8bc6d896651fa03ce7eec72f88df20b8fe02
clarinet758 <clarinet758@gmail.com>
2020-03-03 23:14:43 +0900

    dp

--- abc/b001_025/b007/d1.py
+++ abc/b001_025/b007/d1.py
@@ -1,12 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-a,b=map(int,raw_input().split())
-ans=0
-if b>10000:
-    print 0
-    exit()
-for i in range(a,b+1):
-    i=str(i)
-    if '4' in i or '9' in i:
-        ans+=1
-print ans
+
+def sol(f):
+    s="0"+str(f)
+    l=[int(i) for i in s]
+    dp1=[0]*len(s)
+    dp2=[0]*len(s)
+    for i,j in enumerate(l):
+        if j not in [4,9]:
+            dp1[i]=1
+        else:
+            break
+    for i,j in enumerate(l):
+        if i==0: continue
+        x=y=0
+        if dp1[i]==1 or dp1[i-1]+dp1[i]==1:
+            y=l[i]-(l[i]>4)
+        dp2[i]=(dp2[i-1]*8)+(x*8)+y
+    return f-(dp1[-1]+dp2[-1]-1)
+            
+a,b=map(int,input().split())
+print(sol(b)-sol(a-1))
