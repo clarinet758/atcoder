6655f66bfe58051018ec51fb4749439b00ced966
clarinet758 <clarinet758@gmail.com>
2020-03-08 01:00:39 +0900

    dp

--- abc/b001_025/b007/d1.py
+++ abc/b001_025/b007/d1.py
@@ -3,45 +3,32 @@
 
 
 def cnt(x):
-    sq=[4,9]
-    l=[int(i) for i in "0"+str(x)]
-    # [0]=n  [1]<n
-    dp=[[[0]*2,[0]*2] for _ in "xx"]
+    dp=[[[0,0],[0,0]] for _ in "ww"]
     dp[0][0][0]=1
-    for i in range(1,len(l)):
-        j=l[i]
-        if dp[i%2-1][0][1] or j in sq:
+    l=[int(i) for i in "0"+str(x)]
+    for i,j in enumerate(l):
+        if i==0: continue
+        if dp[i%2-1][0][1] or j in (4,9):
             dp[i%2][0][1]=1
         else:
             dp[i%2][0][0]=1
 
         if dp[i%2-1][0][0]:
-            for k in range(j):
-                if k==4: dp[i%2][1][1]+=1
-                else: dp[i%2][1][0]+=1
-        else:
+            dp[i%2][1][1]+=[0,1][j>4]
+            dp[i%2][1][0]+=[j,j-1][j>4]
+        if dp[i%2-1][0][1]:
             dp[i%2][1][1]+=j
 
         if dp[i%2-1][1][1]:
             dp[i%2][1][1]+=dp[i%2-1][1][1]*10
-
         if dp[i%2-1][1][0]:
-            dp[i%2][1][0]+=dp[i%2-1][1][0]*8
             dp[i%2][1][1]+=dp[i%2-1][1][0]*2
-                    
+            dp[i%2][1][0]+=dp[i%2-1][1][0]*8
+        for p in range(4):
+            dp[i%2-1][p//2][p%2]=0
 
-        for k in range(4):
-            aa,bb=k//2,k%2
-            dp[i%2-1][aa][bb]=0
-    r=0
-    for k in range(4):
-        aa,bb=k//2,k%2
-        r+=dp[aa][bb][-1]
+    return dp[0][0][1]+dp[1][0][1]+dp[0][1][1]+dp[1][1][1]
     
-    #for k in dp:
-    #    print(k)
-    return r
-
 a,b=map(int,input().split())
 a-=1
 print(cnt(b)-cnt(a))
