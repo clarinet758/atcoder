4bbd072761c7144ad1a323bb8b638f20c58acfd7
clarinet758 <clarinet758@gmail.com>
2020-08-21 00:47:20 +0900

    初級ゴール！！

--- abc/b051_075/b054/c1.py
+++ abc/b051_075/b054/c1.py
@@ -1,50 +1,19 @@
-#!/usr/bin/env pypy3
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-import sys
-import re
-import math
-import itertools
-import collections
-import bisect
-#sys.stdin=file('input.txt')
-#sys.stdout=file('output.txt','w')
-#10**9+7
-mod=1000000007
-#mod=1777777777
-pi=3.1415926535897932
-IS=float('inf')
-xy=[(1,0),(-1,0),(0,1),(0,-1)]
-bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
-def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
-def fib(n): return [(seq.append(seq[i-2] + seq[i-1]), seq[i-2])[1] for seq in [[0, 1]] for i in range(2, n)]
-def gcd(a,b): return a if b==0 else gcd(b,a%b)
-def lcm(a,b): return a*b/gcd(a,b)
-def eucl(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
-def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
-def pscl(num,l=[1]):
-    for i in range(num):
-        l = map(lambda x,y:x+y,[0]+l,l+[0])
-    return l
 
 import itertools
-
 n,m=map(int,input().split())
-path={i+1:[] for i in range(n)}
+l=[[0]*n for i in range(n)]
 for i in range(m):
-    u,v=map(int,input().split())
-    path[u].append(v)
-    path[v].append(u)
-chk=[i for i in range(2,n+1)]
-ans=0
-for way in list(itertools.permutations(chk)):
-    now=1
-    if way[0] in path[now]:
-        f=1
-        for j in way:
-            if j in path[now]:
-                now=j
-                f+=1
-            else:
-                break
-        if f==n: ans+=1
+    a,b=map(int,input().split())
+    l[a-1][b-1]=1
+    l[b-1][a-1]=1
+d=[int(i)+1 for i in range(n)]
+ans=chk=0
+for i in list(itertools.permutations(d,n)):
+    chk=1
+    if i[0]!=1: continue
+    for j in range(n-1):
+        chk*=l[i[j+1]-1][i[j]-1]
+    ans+=chk
 print(ans)
