68abf57a18302a2f29809d385fdfe28b5191969e
clarinet758 <clarinet758@gmail.com>
2020-09-11 02:46:45 +0900

    ho

--- abc/b126_150/b126/d1.py
+++ abc/b126_150/b126/d1.py
@@ -1,47 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-from collections import deque
-def sol():
-    n=int(input())
-    d=[-1]*n
-    l=deque([])
-    for i in range(n-1):
-        u,v,w=map(int,input().split())
-        u,v=min(u,v),max(u,v)
-        l.append((u-1,v-1,w))
-    f=0
-    while len(l):
-        x=l.popleft()
-        if f:
-            if x[2]%2 and d[x[0]]==0:
-                d[x[1]]=1
-            elif x[2]%2 and d[x[0]]==1:
-                d[x[1]]=0
-            elif x[2]%2==0 and d[x[0]]==1:
-                d[x[1]]=1
-            elif x[2]%2==0 and d[x[0]]==0:
-                d[x[1]]=0
-            elif x[2]%2 and d[x[1]]==0:
-                d[x[0]]=1
-            elif x[2]%2 and d[x[1]]==1:
-                d[x[0]]=0
-            elif x[2]%2==0 and d[x[1]]==1:
-                d[x[0]]=1
-            elif x[2]%2==0 and d[x[1]]==0:
-                d[x[0]]=0
-            else:
-                l.append(x)
-        else:
-            f=1
-            if x[2]%2:
-                d[x[0]]=0
-                d[x[1]]=1
-            else:
-                d[x[0]]=0
-                d[x[1]]=0
-    for i in d:
-        print(i)
+import collections
+n=int(input())
+d=[2]*n
+w=collections.deque()
+for i in range(n-1):
+    a,b,c=map(int,input().split())
+    if i==0:
+        d[a-1]=0
+        d[b-1]=c%2
+    elif d[a-1]!=2:
+        d[b-1]=(d[a-1]+c%2)%2
+    elif d[b-1]!=2:
+        d[a-1]=(d[b-1]+c%2)%2
+    else:
+        w.append((a,b,c))
 
-if __name__=="__main__":
-    sol()
+while len(w):
+    a,b,c=w.pop()
+    if d[a-1]!=2:
+        d[b-1]=(d[a-1]+c%2)%2
+    elif d[b-1]!=2:
+        d[a-1]=(d[b-1]+c%2)%2
+    else:
+        w.appendleft((a,b,c))
+for i in d:
+    print(i)
