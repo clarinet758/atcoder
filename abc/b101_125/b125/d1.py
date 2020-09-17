d05d45e7e42b29d50694800b6909acfae6fd1a18
clarinet758 <clarinet758@gmail.com>
2020-09-17 23:49:17 +0900

    hoo

--- abc/b101_125/b125/d1.py
+++ abc/b101_125/b125/d1.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-int(input())
+n=int(input())
 a=[int(i) for i in input().split()]
-b=[abs(int(i)) for i in a]
-c=[int(i) for i in a if i<0]
-print(sum(b)-[0,min(b)*2][len(c)%2])
-
+x=0
+b=[]
+for i in a:
+  if i<0: x=x+1
+  b.append(abs(i))
+b.sort()
+print(sum(b)-b[0]*2 if  x%2 else sum(b))
