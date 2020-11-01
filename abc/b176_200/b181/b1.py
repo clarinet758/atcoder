6db4cad50a47a06e6d3dffa8c0b828703d6708fe
clarinet758 <clarinet758@gmail.com>
2020-11-01 23:30:50 +0900

    sigma

--- abc/b176_200/b181/b1.py
+++ abc/b176_200/b181/b1.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+def sig(a): return a*(a+1)//2
 n=int(input())
-ans=chk=0
+ans=0
 for i in range(n):
     a,b=map(int,input().split())
-    ans+=(b*(b+1)//2)-(a*(a-1)//2)
+    ans+=sig(b)-sig(a-1)
 print(ans)
