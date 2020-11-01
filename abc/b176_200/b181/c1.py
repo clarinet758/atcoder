3fb5a2861bd2d14990e2317a4564863177610703
clarinet758 <clarinet758@gmail.com>
2020-11-01 23:13:58 +0900

    temp

--- abc/b176_200/b181/c1.py
+++ abc/b176_200/b181/c1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+def tri(xa,ya,xb,yb,xc,yc): return abs((xa-xc)*(yb-yc)-(xb-xc)*(ya-yc))
 
 n=int(input())
 w=[]
@@ -11,6 +12,5 @@ ans=1
 for i in range(n-2):
     for j in range(i+1,n-1):
         for k in range(j+1,n):
-            p=(w[i][0]-w[k][0])*(w[j][1]-w[k][1])-(w[j][0]-w[k][0])*(w[i][1]-w[k][1])
-            ans=min(ans,abs(p))
+            ans=min(ans,tri(w[i][0],w[i][1],w[j][0],w[j][1],w[k][0],w[k][1]))
 print("No" if ans else "Yes")
