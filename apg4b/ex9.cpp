ab5900bad21dea54a91187e6a41062dcc562e2d7
clarinet758 <clarinet758@gmail.com>
2022-10-24 02:49:25 +0900

    oh!

--- apg4b/ex9.cpp
+++ apg4b/ex9.cpp
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
   int x, a, b;
   cin >> x >> a >> b;
- 
+
   // 1.の出力
   x++;
   cout << x << endl;
- 
+
   // ここにプログラムを追記
-  x *= (a+b);
-  cout << x << endl;
-  x *= x;
-  cout << x << endl;
-  x--;
-  cout << x << endl;
+  cout << x * (a+b) << endl;
+  cout << (x * (a+b)) * (x * (a+b)) << endl;
+  cout << (x * (a+b)) * (x * (a+b)) - 1 << endl;
 }
