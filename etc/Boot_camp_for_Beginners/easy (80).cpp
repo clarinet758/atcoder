2c1ac69b0fe7d2d74086909a380a77d5e0bb1eee
clarinet758 <clarinet758@gmail.com>
2022-12-20 22:30:57 +0900

    aaa

---
+++ etc/Boot_camp_for_Beginners/easy (80).cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll int64_t
+#define rep(i,n)  for(int i=0;i<n;++i)
+#define per(i,n)  for(int i=n-1;i>=0;--i)
+#define ii1(a)  scanf("%d",&a)
+#define ii2(a,b)  scanf("%d %d",&a,&b)
+#define ii3(a,b,c)  scanf("%d %d %d",&a,&b,&c)
+#define ll1(a)  scanf("%lld",&a)
+#define ll2(a,b)  scanf("%lld %lld",&a,&b)
+#define ll3(a,b,c)  scanf("%lld %lld %lld",&a,&b,&c)
+#define PI 3.1415926535897932
+
+void ww(int n,vector<int> &a) { rep(i,n) ii1(a.at(i)); }
+
+//
+int souwa(int a) {return (1+a)*a/2;}
+int lcm(int a,int b) { return a*b/__gcd(a,b); }
+//long long lcm(ll a,ll b) { return a*b/__gcd(a,b); }
+
+//
+bool sankaku(int a,int b,int c) {vector <int> t={a,b,c};sort(t.begin(),t.end()); return t.at(0)+t.at(1)>t.at(2);};
+
+/** sort(ar.begin(),ar.end())
+    int sum=accumulate(ar.begin(),ar.end(),0); 
+    do {// do内部で作られた順列に対して必要な処理を行う
+        // cout << w.at(0) << w.at(1) << w.at(2) << endl;
+    } while (next_permutation(w.begin(),w.end()));  //ex. vector <int> w= {1,2,3}; **/
+
+// 何か貼るときはココから下に
+
+int main(){
+    int mod=1000000007;
+    ll n,m,ans=1e12+5;
+    bool f=1;
+    //scanf("%d %d",&n,&m);
+    ll1(n);
+    vector <ll> p(1,2ll);
+    for (ll i=3ll;i<1000005ll;i++) {
+        for (int j=0;j<p.size();j++) {
+            if (i%p.at(j)==0ll) break;
+            if (p.at(j)*p.at(j)>i) {
+                p.push_back(i);
+                break;
+            }
+        }
+    }
+    for (ll i=1ll;i*i<n;i++) {
+        if (n%i==0) {
+            ans=min(ans,(i-1)+((n/i)-1));
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
