3ce2bb5d98a2469181fbb3f65e0bae17cd275fed
clarinet758 <clarinet758@gmail.com>
2023-09-09 02:52:51 +0900

    oooo

--- abc/b151_175/b166/d1.cpp
+++ abc/b151_175/b166/d1.cpp
@@ -10,42 +10,54 @@ using namespace std;
 #define sl2(a,b)  scanf("%lld %lld",&a,&b)
 #define sl3(a,b,c)  scanf("%lld %lld %lld",&a,&b,&c)
 #define PI 3.1415926535897932
+#define print(a) cout << a << endl
+#define pp puts("")
+
+#define Yes printf("Yes\n")
+#define No printf("No\n")
+void yneso(int a) {if(a) cout << Yes; else cout << No;}
+
+//int64_t はatcoderメリット不明のため long long
+typedef long long ll;
+//#define ll int64_t
 
 //
 int souwa(int a) {return (1+a)*a/2;}
 int lcm(int a,int b) { return a*b/__gcd(a,b); }
-//long long lcm(ll a,ll b) { return a*b/__gcd(a,b); }
+ll gcdll(ll a, ll b) { if(b==0ll) return a; else return gcdll(b, a%b); }
+ll lcmll(ll a,ll b) { return (a/gcdll(a,b)*b); }
+ll maxll(ll a,ll b) {if(a>b){return a;}else{return b;}}
+ll minll(ll a,ll b) {if(a<b){return a;}else{return b;}}
 
-//
+double tilt(int x1,int y1,int x2,int y2) {return (1.0*y2-1.0*y1)/(1.0*x2-1.0*x1);}
+double tri(int xa,int ya,int xb,int yb,int xc,int yc) {return (1.0*xa-1.0*xc)*(1.0*yb-1.0*yc)-(1.0*xb-1.0*xc)*(1.0*ya-1.0*yc);}
 bool sankaku(int a,int b,int c) {vector <int> t={a,b,c};sort(t.begin(),t.end()); return t.at(0)+t.at(1)>t.at(2);};
 
 /** sort(ar.begin(),ar.end())
+ * vector<vector<int>> a(5,vector<int>)
     int sum=accumulate(ar.begin(),ar.end(),0); 
     do {// do内部で作られた順列に対して必要な処理を行う
+        //必ず事前にソートしておくこと、配列の中身を見ていて？辞書順に次のもの次のもの作成するのでソートしておかないと列挙が漏れる
         // cout << w.at(0) << w.at(1) << w.at(2) << endl;
     } while (next_permutation(w.begin(),w.end()));  //ex. vector <int> w= {1,2,3}; **/
 
 // 何か貼るときはココから下に
 
 int main(){
-    int n,q,m,ans;
-    sc2(n,q);
-    vector <int> a(n);
-    map <int,vector<int>> w;
-    rep(i,n) {
-        int p;
-        sc1(p);
-        a.at(i)=p;
-        w[p].push_back(i+1);
-    }
-    rep(i,q) {
-        int x,k;
-        sc2(x,k);
-        if (w[x].size()<k) {
-            printf("-1\n");
-        } else {
-            printf("%d\n",w[x].at(k-1));
+    int mod=1e9+7;
+    ll n,k,x,y,z,cnt=0,ans=0;
+    cin >> x;
+    vector<ll> w(300);
+    rep(i,300) w.at(i)=1ll*i*i*i*i*i;
+    for(int i=0;i<300;i++){
+        for(int j=i;j<300;j++){
+            ll a,b;
+            b=w.at(i);a=w.at(j);
+            if(a-b==x) {cout << j << " " << i << endl; return 0;}
+            if(a+b==x) {cout << j << " " << -i << endl; return 0;}
+            if(-a+b==x) {cout << -j << " " << -i << endl; return 0;}
+            if(-a-b==x) {cout << -j << " " << i << endl; return 0;}
         }
     }
-    return 0;
+    return 1;
 }
