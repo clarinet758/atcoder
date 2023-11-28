84f2145ce08e48961b0a982969ffa590222cbf7e
clarinet758 <clarinet758@gmail.com>
2023-11-29 02:04:22 +0900

    ad

--- etc/etc/edpc/e1.cpp
+++ etc/etc/edpc/e1.cpp
@@ -1,23 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define rep(i,n)  for(int i=0;i<n;++i)
+#define per(i,n)  for(int i=n-1;i>=0;--i)
+#define sc1(a)  scanf("%d",&a)
+#define sc2(a,b)  scanf("%d %d",&a,&b)
+#define sc3(a,b,c)  scanf("%d %d %d",&a,&b,&c)
+#define sl1(a)  scanf("%lld",&a)
+#define sl2(a,b)  scanf("%lld %lld",&a,&b)
+#define sl3(a,b,c)  scanf("%lld %lld %lld",&a,&b,&c)
+#define PI 3.1415926535897932
+#define print(a) cout << a << endl
+#define pp puts("")
+
+#define Yes printf("Yes\n")
+#define No printf("No\n")
+void yneso(int a) {if(a) cout << Yes; else cout << No;}
+
+//int64_t はatcoderメリット不明のため long long
+typedef long long ll;
+//#define ll int64_t
+
+//
+int souwa(int a) {return (1+a)*a/2;}
+int lcm(int a,int b) { return a*b/__gcd(a,b); }
+ll gcdll(ll a, ll b) { if(b==0ll) return a; else return gcdll(b, a%b); }
+ll lcmll(ll a,ll b) { return (a/gcdll(a,b)*b); }
+ll maxll(ll a,ll b) {if(a>b){return a;}else{return b;}}
+ll minll(ll a,ll b) {if(a<b){return a;}else{return b;}}
+
+double tilt(int x1,int y1,int x2,int y2) {return (1.0*y2-1.0*y1)/(1.0*x2-1.0*x1);}
+double tri(int xa,int ya,int xb,int yb,int xc,int yc) {return (1.0*xa-1.0*xc)*(1.0*yb-1.0*yc)-(1.0*xb-1.0*xc)*(1.0*ya-1.0*yc);}
+bool sankaku(int a,int b,int c) {vector <int> t={a,b,c};sort(t.begin(),t.end()); return t.at(0)+t.at(1)>t.at(2);};
+
+/** sort(ar.begin(),ar.end())
+ * vector<vector<int>> a(5,vector<int>)
+    int sum=accumulate(ar.begin(),ar.end(),0); 
+    do {// do内部で作られた順列に対して必要な処理を行う
+        //必ず事前にソートしておくこと、配列の中身を見ていて？辞書順に次のもの次のもの作成するのでソートしておかないと列挙が漏れる
+        // cout << w.at(0) << w.at(1) << w.at(2) << endl;
+    } while (next_permutation(w.begin(),w.end()));  //ex. vector <int> w= {1,2,3}; **/
+
+// 何か貼るときはココから下に
+
 int main(){
-    int mod=1000000007;
-    int n,w,ans=0;
-    scanf("%d %d",&n,&w);
-    int x=n*1000+1;
-    int dp[x];
-    for (int i=1;i<x;i++) dp[i]=mod;
-    for (int i=0;i<n;i++) {
-        int a,b;
-        scanf("%d %d",&a,&b);
-        for (int j=x-1-b;j>0;j--) if (dp[j]<mod) dp[j+b]=min(dp[j+b],dp[j]+a);
-        dp[b]=min(dp[b],a);
+    ll mod=(1e9+7)*1ll;
+    ll n,w,x,y,ans=0;
+    cin >> n >> w;
+    vector<vector<ll>> dp(n+1,vector<ll>(100005,100ll*mod)); //dp[i][j]=k  i番目までの品物を選択候補にしたときに、価値jを実現する最小の重さがk
+    dp[0][0]=0; //0状態で価値0を実現するのは重さ0
+    rep(i,n) {
+        cin >> x >> y;
+        for(int j=0;j<100005;j++){
+            if(j-y>=0) dp[i+1][j]=minll(dp[i+1][j],dp[i][j-y]+x); //dp[i+1][j]を実現する重さを、dp[i][j-y]+x(1つ手前のi番目で価値j-yを実現している重さに今回の重さxを足した値と比較する)
+            dp[i+1][j]=minll(dp[i+1][j],dp[i][j]);
+        }
     }
-    for (int i=x-1;i>0;i--) if (dp[i]<=w) {
-        ans=i;
-        break;
-    }
-    printf("%d\n",ans);
+    rep(i,100005) if(dp[n][i]<=w) ans=i;
+    cout << ans << endl;
     return 0;
 }
